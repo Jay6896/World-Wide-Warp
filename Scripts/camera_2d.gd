b670d810d9e2b20bec8235093d80d2edde0fec71
Jay6896 <jamilsalami08@gmail.com>
2026-05-01 19:24:42 +0100

    chore(files): change file names and hierarchy

--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -1,16 +1,15 @@
 extends Camera2D
 
-@onready var character_pos = $"../CharacterBody2D"
-@onready var camera_pos = $Camera2D
+@onready var character_pos = $"../Player/CharacterBody2D"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	position.x = character_pos.position.x
+	position.x = character_pos.global_position.x
 	position.y = 340.0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x = character_pos.position.x
+	position.x = character_pos.global_position.x
 	position.y = 340.0
